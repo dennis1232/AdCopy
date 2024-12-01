6e8d1cc120643585094e810dbea222c2301181d5
dennis1232 <dennisn@webiya.co.il>
2024-12-01 21:34:56 +0200

    fix build

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import InputField from '../components/InputField'
 import TextAreaField from '../components/TextAreaField'
 import axios from 'axios'
